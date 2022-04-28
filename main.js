bf332bbffd8c78da54406917c65c1567f824c6a5
SpookyTryhards <94879186+SpookyTryhards@users.noreply.github.com>
2022-04-28 17:35:12 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,26 +1,102 @@
-song = "";
+song1 = "";
 song2 = "";
 
+song1_status = "";
+song2_status = "";
 
-function setup(){
-canvas = createCanvas(600, 500);
-canvas.center();
+scoreOfRightWrist = 0;
+scoreOfLeftWrist = 0;
 
-video = createCapture(VIDEO);
-video.hide();
+rightWristX = 0;
+rightWristY = 0;
+
+leftWristX = 0;
+leftWristY = 0;
+
+function preload()
+{
+	song1 = loadSound("music.mp3");
+	song2 = loadSound("music2.mp3");
 }
 
+function setup() {
+	canvas =  createCanvas(600, 600);
+	canvas.center();
 
-function preload(){
-song = loadSound("music.mp3");
-song2 = loadSound("music2.mp3")
+	video = createCapture(VIDEO);
+	video.hide();
+
+	poseNet = ml5.poseNet(video, modelLoaded);
+	poseNet.on('pose', gotPoses);
 }
 
-function draw(){
-    image(video, 0, 0, 600, 500);
+function modelLoaded() {
+  console.log('PoseNet Is Working!');
 }
 
+function gotPoses(results)
+{
+  if(results.length > 0)
+  {
+	console.log(results);
+	scoreOfRightWrist =  results[0].pose.keypoints[10].score;
+	scoreOfLeftWrist =  results[0].pose.keypoints[9].score;
+	console.log("scoreRightWrist = " + scoreRightWrist + "scoreLeftWrist = " + scoreLeftWrist);
+	
+	rightWristX = results[0].pose.rightWrist.x;
+	rightWristY = results[0].pose.rightWrist.y;
+	console.log("rightWristX = " + rightWristX +" rightWristY = "+ rightWristY);
 
-  
+	leftWristX = results[0].pose.leftWrist.x;
+	leftWristY = results[0].pose.leftWrist.y;
+	console.log("leftWristX = " + leftWristX +" leftWristY = "+ leftWristY);
+		
+  }
+}
+
+function draw() {
+	image(video, 0, 0, 600, 500);
+	
+	song1_status = song1.isPlaying();
+	song2_status = song2.isPlaying();
+
+	fill("#FF0000");
+	stroke("#FF0000");
+
+	if(scoreRightWrist > 0.2)
+	{ 
+		circle(rightWristX,rightWristY,20);
+
+			song2.stop();
+
+		if(song1_status == false)
+		{
+			song1.play();
+			document.getElementById("song").innerHTML = "Harry Potter Theme Song is Playing"
+		}
+	}
+
+	if(scoreLeftWrist > 0.2)
+	{
+		circle(leftWristX,leftWristY,20);
+
+			song1.stop();
+
+		if(song2_status == false)
+		{
+			song2.play();
+			document.getElementById("song").innerHTML = "Peter Pan Song is Playing"
+		}
+	}
+
+}
+
+function play()
+{
+	song.play();
+	song.setVolume(1);
+	song.rate(1);
+}
+
 
 
