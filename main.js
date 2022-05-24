5bc97e7cd39a2353466f3242b9cf153b6292f174
SpookyTryhards <94879186+SpookyTryhards@users.noreply.github.com>
2022-05-24 18:26:08 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
-song1 = "";
+song = "";
 song2 = "";
 
-song1_status = "";
+song_status = "";
 song2_status = "";
 
-scoreOfRightWrist = 0;
-scoreOfLeftWrist = 0;
+scoreRightWrist = 0;
+scoreLeftWrist = 0;
 
 rightWristX = 0;
 rightWristY = 0;
@@ -15,12 +15,12 @@ leftWristY = 0;
 
 function preload()
 {
-	song1 = loadSound("music.mp3");
+	song = loadSound("music.mp3");
 	song2 = loadSound("music2.mp3");
 }
 
 function setup() {
-	canvas =  createCanvas(600, 600);
+	canvas =  createCanvas(600, 500);
 	canvas.center();
 
 	video = createCapture(VIDEO);
@@ -31,7 +31,7 @@ function setup() {
 }
 
 function modelLoaded() {
-  console.log('PoseNet Is Working!');
+  console.log('PoseNet Is Initialized');
 }
 
 function gotPoses(results)
@@ -39,8 +39,8 @@ function gotPoses(results)
   if(results.length > 0)
   {
 	console.log(results);
-	scoreOfRightWrist =  results[0].pose.keypoints[10].score;
-	scoreOfLeftWrist =  results[0].pose.keypoints[9].score;
+	scoreRightWrist =  results[0].pose.keypoints[10].score;
+	scoreLeftWrist =  results[0].pose.keypoints[9].score;
 	console.log("scoreRightWrist = " + scoreRightWrist + "scoreLeftWrist = " + scoreLeftWrist);
 	
 	rightWristX = results[0].pose.rightWrist.x;
@@ -57,7 +57,7 @@ function gotPoses(results)
 function draw() {
 	image(video, 0, 0, 600, 500);
 	
-	song1_status = song1.isPlaying();
+	song_status = song.isPlaying();
 	song2_status = song2.isPlaying();
 
 	fill("#FF0000");
@@ -69,10 +69,10 @@ function draw() {
 
 			song2.stop();
 
-		if(song1_status == false)
+		if(song_status == false)
 		{
-			song1.play();
-			document.getElementById("song").innerHTML = "Harry Potter Theme Song is Playing"
+			song.play();
+			document.getElementById("song1").innerHTML = "Playing - Harry Potter Theme Song"
 		}
 	}
 
@@ -80,12 +80,12 @@ function draw() {
 	{
 		circle(leftWristX,leftWristY,20);
 
-			song1.stop();
+			song.stop();
 
 		if(song2_status == false)
 		{
 			song2.play();
-			document.getElementById("song").innerHTML = "Peter Pan Song is Playing"
+			document.getElementById("song").innerHTML = "Playing - Peter Pan Song"
 		}
 	}
 
@@ -97,6 +97,9 @@ function play()
 	song.setVolume(1);
 	song.rate(1);
 }
+
+
+
 
 
 
